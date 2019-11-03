37bc1d040a4db304036a0d30650ef5d5008d491f
Kevin <anyboo@qq.com>
2019-11-04 07:48:19 +0800

    choice by sql

--- routes/heartbeat.js
+++ routes/heartbeat.js
@@ -4,7 +4,10 @@ import {
   check, validationResult
 }
 from 'express-validator'
-
+import sqlite from 'sqlite'
+const dbPromise = sqlite.open('./db/teaServer.db', {
+  Promise
+})
 let trace = debug('teaServ:heartbeat')
 let router = express.Router();
 
@@ -27,23 +30,60 @@ router.post('/', [
     })
   }
 
-  if (req.query.cpuid != "") {
-    let _voice = `欢迎${req.query.cpuid}光临茶室`;
-    return res.status(200).json({
-      code: 1,
-      msg: "请求成功",
-      voice: _voice,
-      door: 0,
-      air: 0,
-      socket: 0,
-      lamp: 0
-    });
+  const status = room_getstatus(req.query.cpuid);
+  status.then((room) => {
+    if (typeof room == 'object' && 'cpuid' in room) {
+      return res.status(200).json({
+        code: 0,
+        msg: '请求成功',
+        voice: '',
+        door: room.door,
+        lamp: room.lamp,
+        socket: room.socket,
+        air: room.air
+      })
+    } else return res.status(200).json({
+      code: 0,
+      msg: "请求失败"
+    })
+  });
+
+  // if (req.query.cpuid != "") {
+  //   let _voice = `欢迎${req.query.cpuid}光临茶室`;
+  //   return res.status(200).json({
+  //     code: 1,
+  //     msg: "请求成功",
+  //     voice: _voice,
+  //     door: 0,
+  //     air: 0,
+  //     socket: 0,
+  //     lamp: 0
+  //   });
+  // }
+  //
+  // return res.status(200).json({
+  //   code: 0,
+  //   msg: "请求失败"
+  // })
+});
+
+
+async function room_getstatus(uuid) {
+  const db = await dbPromise;
+  const [room] = await Promise.all([
+    db.get(
+      `SELECT name,cpuid, door,lamp,socket,air FROM room WHERE cpuid = ?`,
+      uuid
+    )
+  ]);
+
+  trace('room_getstatus select:', typeof room, room);
+
+  if (typeof room == 'object' && 'cpuid' in room) {
+    return room
   }
 
-  return res.status(200).json({
-    code: 0,
-    msg: "请求失败"
-  })
-});
+  return [];
+}
 
 module.exports = router;
